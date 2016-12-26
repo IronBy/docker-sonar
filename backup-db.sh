14a32c3d910045503c85524eccc698528b9fe659
Valery Sushko <v.sushko@itransition.com>
2016-12-26 23:18:47 +0300

    Add my sql setting max-allowed-packet=50M; Mount Sonar logs folder

--- backup-db.sh
+++ backup-db.sh
@@ -2,3 +2,7 @@
 # Usage: ./backup-db.sh <username> <password> <database container id or name>
 
 docker run --rm --link $3:db tutum/mysql:5.6 mysqldump -u$1 -p$2 -hdb sonar >backup/$(date +%Y%m%d-%H%M)-sonar.sql
+
+# For Windows users
+# docker network might be obtainerd via docker network ls
+# docker run --rm --link <sonar db container id or name>:db --net <docker network> tutum/mysql:5.6 mysqldump -uroot -p123456 -hdb sonar >backup/db-sonar.sql
